6ad54ad6ae7e1efe3c0d5d7e9b9ac98ed7fd2cee
nychavs <chavesnaiury@gmail.com>
2022-10-26 15:36:26 -0300

    adding register validation on frontend

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,15 +1,59 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import styled from "styled-components";
-import Logo from '../assets/logo.svg'
+import Logo from '../assets/logo.svg';
+import {ToastContainer, toast} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Register(){
-    const handleSubmit = (event) =>{
-        event.preventDefault();
-        alert("form")
+    const [values, setValues] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    })
+    const toastOptions = {
+        position: "bottom-right",
+            autoClose:8000,
+            pauseOnHover:true,
+            draggable:true,
+            theme:"dark"
     }
 
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        handleValidation()
+    }
+
+        // const notify = () => toast.error("Wow so easy!", toastOptions);
+        // return (
+        //   <div>
+        //     <button onClick={notify}>Notify!</button>
+        //     <ToastContainer />
+        //   </div>
+        // );
+      
+    const handleValidation = () =>{
+        const {password, confirmPassword, username, email} = values;
+
+        if (password !== confirmPassword){
+            console.log("iyhcfif")
+            toast.error("password and confirm password should be the same", toastOptions);
+            return false;
+        } else if (username.length < 3){
+            toast.error("Username should be greater than 3 characters", toastOptions);
+            return false;
+        } else if (password.length < 8){
+            toast.error("Password should be equal or  greater than 3 characters", toastOptions);
+            return false;
+        } else if (email===""){
+            toast.error("email is required", toastOptions);
+            return false;
+        }
+        return true;
+}
     const handleChange=(event) =>{
+        setValues({...values, [event.target.name]: event.target.value });
     }
   return (
   <>
@@ -28,6 +72,7 @@ export default function Register(){
         <span>Already have an account? <Link to="/login">Login</Link></span>
     </form>
   </FormContainer>
+  <ToastContainer />
   </>
 )}
 
@@ -61,6 +106,7 @@ const FormContainer = styled.div`
         background-color: #00000076;
         border-radius: 2rem;
         padding: 3rem 5rem;
+
         input{
             background-color: transparent;
             padding:1rem;
@@ -69,11 +115,35 @@ const FormContainer = styled.div`
             color: white;
             width: 100%;
             font-size: 1rem;
+
             &:focus{
                 border: 0.1rem solid #997af0;
-                color: white;
-                padding: 1rem 2rem;
+                outline: none
             }
         }
+        button {
+        background-color: #997af0;
+        color: white;
+        padding: 1rem 2rem;
+        font-weight: bold;
+        cursor: pointer;
+        border-radius: 0.4rem;
+        font-size: 1rem;
+        text-transform: uppercase;
+        transition: 0.5s ease-in-out;
+            $:hover{
+                backgoround-color: #4e0eff;
+            }
+        }
+        span{
+            color: white;
+            text-transform: uppercase;
+            a{
+                color: #997af0;
+                text-decoration: none;
+                font-weight: bold;
+            }
+        }
+
     }
 `;
